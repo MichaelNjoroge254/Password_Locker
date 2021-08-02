c49b55927d54f22e74425f2f4eef5de423794ae8
MichaelNjoroge254 <michael.njoroge@student.moringaschool.com>
2021-08-02 11:38:34 +0300

    Modified run.py

--- run.py
+++ run.py
@@ -113,6 +113,8 @@ def main():
 
                             print("password ...")
                             password = input()
+                            print("confirm password ...")
+                            password = input()
                             save_creds(create_creds(uname,password)) # create and save credentials
                             print ('\n')
                             print(f"Your new account with username : '{uname}' and password '{password}' has been successfully created")
